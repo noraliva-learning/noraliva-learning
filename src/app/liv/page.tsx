b2c1206c0b69751ec06c4c2bf7334dc261e473ef
noraliva-learning <drcolettenichols@gmail.com>
2026-02-25 16:27:59 -0800

    Add /liv route

---
+++ src/app/liv/page.tsx
@@ -0,0 +1,8 @@
+export default function LivPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 32, fontWeight: 700 }}>Liv</h1>
+      <p>Liv’s learning dashboard will live here.</p>
+    </main>
+  );
+}
