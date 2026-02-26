d09c35c47f51169902325e769af1d1d51089495d
noraliva-learning <drcolettenichols@gmail.com>
2026-02-25 16:31:53 -0800

    Add /learners/liv route

---
+++ src/app/learners/liv/page.tsx
@@ -0,0 +1,8 @@
+export default function LivLearnerPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 32, fontWeight: 700 }}>Liv</h1>
+      <p style={{ marginTop: 12 }}>Learner page is live. Next: lesson path + domains.</p>
+    </main>
+  );
+}
